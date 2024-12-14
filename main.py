0a753f1e875f191d505f3cd37cea70ea3f7381d3
AirGuy <airguy-one@yandex.ru>
2024-12-14 23:44:01 +0300

    some(2)

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import requests
 
-from fastapi import FastAPI
-
 app = FastAPI()
+
+# Add CORS middleware to allow requests from any origin
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all origins, adjust if needed
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allow all headers
+)
 
 
 @app.get('/')
@@ -33,6 +42,7 @@ async def get_market_caps():
     url = 'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,tether,binancecoin,solana,xrp,cardano,avalanche-2&vs_currencies=usd&include_market_cap=true'
     res = requests.get(url)
     data = res.json()
+
     prices = {
         'Bitcoin': data['bitcoin']['usd_market_cap'],
         'Ethereum': data['ethereum']['usd_market_cap'],
@@ -43,4 +53,3 @@ async def get_market_caps():
         'Avalanche': data['avalanche-2']['usd_market_cap'],
     }
     return prices
-
