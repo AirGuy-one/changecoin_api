74c8d57a108a547e948af1f50cedbe06346a1923
AirGuy <airguy-one@yandex.ru>
2024-12-14 22:56:01 +0300

    some(1)

--- main.py
+++ main.py
@@ -12,11 +12,12 @@ async def root():
 
 @app.get('/last_prices')
 async def get_last_prices():
-    url = 'https://api.coingecko.com/api/v3/simple/price?ids=ethereum,tether,binancecoin,solana,xrp,cardano,avalanche-2&vs_currencies=usd'
+    url = 'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,tether,binancecoin,solana,xrp,cardano,avalanche-2&vs_currencies=usd'
     res = requests.get(url)
     data = res.json()
 
     prices = {
+        'Bitcoin': data['bitcoin']['usd'],
         'Ethereum': data['ethereum']['usd'],
         'Tether': data['tether']['usd'],
         'Binance Coin': data['binancecoin']['usd'],
@@ -29,5 +30,17 @@ async def get_last_prices():
 
 @app.get('/market_caps')
 async def get_market_caps():
-    return {'message': 'Hello World'}
+    url = 'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,tether,binancecoin,solana,xrp,cardano,avalanche-2&vs_currencies=usd&include_market_cap=true'
+    res = requests.get(url)
+    data = res.json()
+    prices = {
+        'Bitcoin': data['bitcoin']['usd_market_cap'],
+        'Ethereum': data['ethereum']['usd_market_cap'],
+        'Tether': data['tether']['usd_market_cap'],
+        'Binance Coin': data['binancecoin']['usd_market_cap'],
+        'Solana': data['solana']['usd_market_cap'],
+        'Cardano': data['cardano']['usd_market_cap'],
+        'Avalanche': data['avalanche-2']['usd_market_cap'],
+    }
+    return prices
 
